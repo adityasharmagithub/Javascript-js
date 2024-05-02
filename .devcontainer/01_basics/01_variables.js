d78cc879eb2a9cfb2a2e0fab8c5db719abea32b5
adityasharmagithub <159531728+adityasharmagithub@users.noreply.github.com>
2024-05-02 05:19:30 +0000

    dataTypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2111981287
+let accountEmail = "aditya1287.be21@chitkarauniversity.edu.in"
+var accountPassword = "12345"
+accountCity = "Pathankot"
+let accountState
+
+
+accountEmail = "sharma1287.be21@chitkarauniversity.edu.in"
+accountPassword = "1234455"
+accountCity = "Gurdaspur"
+
+console.log(accountId)
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log(3+4)
+
+console.log("Aditya")
+
+let name = "Aditya"
+let age = 21
+let isLoggedIn = true
+let state;
+
+console.log(typeof "Aditya")
