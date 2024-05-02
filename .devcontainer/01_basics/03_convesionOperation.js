da56d7b38860dbd279c2a0e568496167b434111d
adityasharmagithub <159531728+adityasharmagithub@users.noreply.github.com>
2024-05-02 06:40:37 +0000

    conversion Operation

---
+++ .devcontainer/01_basics/03_convesionOperation.js
@@ -0,0 +1,19 @@
+let score = "33qwe"
+
+//console.log(typeof score);
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
