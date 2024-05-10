3020b3cf8d7bfc1f14becfb6d9a1b2380ff66c30
adityasharmagithub <159531728+adityasharmagithub@users.noreply.github.com>
2024-05-10 13:24:16 +0000

    data summary

---
+++ datatypes-summary.js
@@ -0,0 +1,34 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = ('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 1234567890n
+
+// Non - primitive / Reference data type 
+
+// Array , Objects , Function
+
+//1. Array :-
+const heros = ["Shaktiman" , "Doremon" , "Ninja Hathori" ];
+
+//2. Objects
+
+let myObj = {
+    name: "Aditya"
+    age: 21,
+}
+
+//3. Functions
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
